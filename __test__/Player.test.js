ca9d9779e576d6837872053d674cf2cbc5913bc2
Idzel Jaimes <irjaimes@outlook.com>
2021-02-13 20:19:00 -0800

    added mock for potion and refactored player constructor

---
+++ __test__/Player.test.js
@@ -0,0 +1,20 @@
+const { expect } = require('@jest/globals');
+const Player = require('../lib/Player.js');
+
+const Potion = require('../lib/Potion');
+
+jest.mock('../lib/Potion');
+
+console.log(new Potion());
+
+
+test('create a player object', () => {
+    const player = new Player('Dave');
+
+    expect(player.name).toBe('Dave');
+    expect(player.health).toEqual(expect.any(Number));
+    expect(player.strength).toEqual(expect.any(Number));
+    expect(player.agility).toEqual(expect.any(Number));
+    // add logic to test inventory object
+    expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
+});
